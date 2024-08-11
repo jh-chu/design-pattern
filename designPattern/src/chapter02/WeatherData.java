7ec07f5429e2f1a81d24dceafb6e16af3e3fb6fe
Chu <cnwlsguq1@naver.com>
2024-08-11 16:18:37 +0900

    GoF 디자인패턴 chapter02 커밋 (옵저버 패턴)

---
+++ designPattern/src/chapter02/WeatherData.java
@@ -0,0 +1,45 @@
+package chapter02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherData implements Subject {
+	
+	private List<Observer> observers;
+	private float temperature;
+	private float humidity;
+	private float pressure;
+	
+	public WeatherData() {
+		observers = new ArrayList<Observer>();
+	}
+	
+	@Override
+	public void registerObserver(Observer o) {
+		observers.add(o);
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		observers.remove(o);
+	}
+
+	@Override
+	public void notifyObservers() {
+		for(Observer observer : observers) {
+			observer.update(temperature, humidity, pressure);
+		}
+	}
+	//측정값이 갱신되면 옵저버에게 알림
+	public void measurementsChanged() {
+		notifyObservers();
+	}
+	
+	public void setMeasurements(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		measurementsChanged();
+	}
+	
+}
