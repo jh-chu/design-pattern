75e570f56e120f06b557c92e8c18459f4123e271
lcm <shaitan9384@gmail.com>
2024-08-18 00:23:06 +0900

    commit

--- designPattern/src/chapter02/WeatherStation.java
+++ designPattern/src/chapter02/WeatherStation.java
@@ -7,6 +7,8 @@ public class WeatherStation {
 		
 		CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
 		
+		HeatIndexDisplay heatDisplay = new HeatIndexDisplay(weatherData);
+		
 		weatherData.setMeasurements(80, 65, 30.4f);
 		weatherData.setMeasurements(82, 70, 29.2f);
 		weatherData.setMeasurements(78, 90, 29.2f);
